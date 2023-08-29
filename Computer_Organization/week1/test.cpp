651eacfa93aae5f0b04952fb53ffcac6082ed909
DongUk-Shin <az9170@naver.com>
2023-08-29 19:20:32 +0900

    객지 1주차

---
+++ Computer_Organization/week1/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "input number: " << endl;
+    int score;
+    cin >> score;
+    cout << score << endl;
+
+    return 0;
+}
