93ab4534f27e8ead1e5e7a8b9dab4805fca64524
DongUk-Shin <az9170@naver.com>
2023-09-09 16:49:21 +0900

    week2

---
+++ OOP/week2/Hangman.cpp
@@ -0,0 +1,22 @@
+/*
+객지 2주차 Hangman Game
+*/
+#include <iostream>
+#include <String>
+using namespace std;
+
+int main() {
+
+    string s = "applev";
+
+    int wordsize= 0;
+    char usercharinput;
+
+    while (wordsize < 7) {
+        cin >> usercharinput;
+    }
+    
+    
+
+    return 0;
+}
